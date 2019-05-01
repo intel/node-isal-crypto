b3516919610ddc689f53e28f91a68689d9b1fd40
Gabriel Schulhof <gabriel.schulhof@intel.com>
2019-05-01 09:42:45 -0700

    test: use process.hrtime() in stream

--- test/stream.js
+++ test/stream.js
@@ -4,24 +4,22 @@ const fs = require('fs');
 const expected = '1feba561bf9106f3cbf6d78dd0c6056eef6ab59f15a30e64530ea6aea91d4e07';
 const digestToString = require('./lib/digestToString');
 const assert = require('assert');
-const hashes = {};
 const crypto = require('crypto');
 const doNodeJS = (process.argv[2] === 'nodejs');
 
 let count = 0;
+const start = process.hrtime();
 const interval = setInterval(() => {
   if (count++ < 100) {
     const input = fs.createReadStream(path.join(__dirname, 'input.txt'));
     const stream = (doNodeJS ? crypto.createHash('sha256') : new SHA256MBStream());
     const x = input.pipe(stream);
     x.on('finish', () => {
-      hashes[x.read().toString('hex')] = true;
+      x.read().toString('hex');
     });
   } else {
+    const elapsed = process.hrtime(start);
+    console.log('elapsed: ' + (elapsed[0] * 1e9 + elapsed[1]) + ' ns');
     clearInterval(interval);
   }
 }, 0);
-
-process.on('exit', () => {
-  console.log(JSON.stringify(hashes, null, 4));
-});
