17b568ee62282d33bfe969b80235332e8310ef6e
Gabriel Schulhof <gabriel.schulhof@intel.com>
2019-05-06 11:30:23 -0700

    benchmark: make various improvements
    - start streams using a busy loop
    - add proper command line support for testing and perf recording

--- benchmark/stream.js
+++ benchmark/stream.js
@@ -1,46 +1,104 @@
 const SHA256MBStream = require('../stream');
 const path = require('path');
 const fs = require('fs');
-const expected = '1feba561bf9106f3cbf6d78dd0c6056eef6ab59f15a30e64530ea6aea91d4e07';
-const assert = require('assert');
 const crypto = require('crypto');
-const doNodeJS = (process.argv[2] === 'nodejs');
-const testCorrectness = (process.argv[3] === 'test');
+const { spawn } = require('child_process');
+const argv = require('yargs')
+  .option('c', {
+    alias: 'count',
+    type: 'number',
+    default: 500,
+    describe: 'How many streams to start'
+  })
+  .option('n', {
+    alias: 'nodejs',
+    type: 'boolean',
+    describe: 'Run Node.js implementation'
+  })
+  .option('t', {
+    alias: 'test',
+    type: 'boolean',
+    describe: 'Record hashes and ignore elapsed time'
+  })
+  .option('w', {
+    alias: 'window-size-percent',
+    type: 'number',
+    default: 0.5,
+    describe: 'Percentage of streams in the middle of the run to measure'
+  })
+  .option('p', {
+    alias: 'perf-length',
+    type: 'number',
+    default: 0,
+    describe: 'Number of seconds of perf data to record'
+  })
+  .help('h')
+  .argv;
+
+if (argv.test) {
+  argv.windowSizePercent = 0;
+}
+
 const results = {
+  streamsDesired: argv.count,
   streamsStarted: 0,
-  streamsDesired: 200,
-  streamsComplete: 0
+  streamsCompleted: 0,
+  streamsMeasured: 0,
+  elapsed: null
 };
-const windowStart = (results.streamsDesired >> 2);
-const windowEnd = windowStart * 3;
-let time;
-const interval = setInterval(() => {
-  if (results.streamsStarted < results.streamsDesired) {
-    results.streamsStarted++;
-    const input = fs.createReadStream(path.join(__dirname, 'input.txt'));
-    const stream = (doNodeJS ? crypto.createHash('sha256') : new SHA256MBStream());
-    const x = input.pipe(stream);
-    x.on('finish', () => {
-      const checksum = x.read().toString('hex');
-      results.streamsComplete++;
-      if (results.streamsComplete === windowStart) {
-        time = process.hrtime();
-      } else if (results.streamsComplete === windowEnd) {
-        time = process.hrtime(time);
-      }
-      if (testCorrectness) {
-        results[checksum] = '';
-      }
-      if (results.streamsComplete === results.streamsDesired) {
-        if (testCorrectness) {
-          console.log(JSON.stringify(results, null, 4));
-        } else {
-          console.log((windowEnd - windowStart + 1) +
-            ' streams in ' + (time[0] * 1e9 + time[1]) + ' ns');
-        }
-      }
-    });
-  } else {
-    clearInterval(interval);
+
+const streamsDesired = 500;
+const windowStart = Math.round(streamsDesired * ((1 - argv.windowSizePercent) / 2));
+const windowEnd = streamsDesired - windowStart;
+const streamsToMeasure = windowEnd - windowStart;
+const inputFile = path.join(__dirname, 'input.txt');
+
+function runPerf(length) {
+  const perf = spawn('perf', [
+    'record', '-F', '99', '-p', '' + process.pid, '-g', '--', 'sleep', ('' + length)
+  ], {
+    stdio: 'inherit'
+  });
+  perf.on('exit', function(number, signal) {
+    console.log('perf exit: ' + number + ', and signal ' + signal);
+  });
+}
+
+function onStreamFinish() {
+  const checksum = this.read().toString('hex');
+  if (argv.test) {
+    results[checksum] = '';
   }
-}, 0);
+  results.streamsCompleted++;
+  if (this._measure) {
+    results.streamsMeasured++;
+    if (results.streamsMeasured === streamsToMeasure &&
+        results.elapsed !== null) {
+      results.elapsed = process
+        .hrtime(results.elapsed)
+        .reduce((first, second) => first * 1e9 + second);
+    }
+  }
+}
+
+for (let streamIndex = 0; streamIndex < streamsDesired; streamIndex++) {
+  if (streamIndex === windowStart) {
+    if (argv.perfLength > 0) {
+      runPerf(argv.perfLength);
+    }
+    results.elapsed = process.hrtime();
+  }
+
+  fs.createReadStream(inputFile)
+    .pipe(argv.nodejs ? crypto.createHash('sha256') : new SHA256MBStream())
+    .on('finish', onStreamFinish)
+    ._measure = (streamIndex >= windowStart && streamIndex < windowEnd);
+  results.streamsStarted++;
+}
+
+process.on('exit', () => {
+  if (argv.test) {
+    delete results.elapsed;
+  }
+  console.log(JSON.stringify(results, null, 4));
+});
--- test/stream.js
+++ test/stream.js
@@ -3,10 +3,12 @@ const { spawnSync } = require('child_process');
 const path = require('path');
 const child = spawnSync(process.execPath, [
   path.resolve(path.join(__dirname, '..', 'benchmark', 'stream.js')),
-  '',
-  'test'
+  '-t',
 ]);
 assert.deepStrictEqual(JSON.parse(child.stdout.toString()), {
   '1feba561bf9106f3cbf6d78dd0c6056eef6ab59f15a30e64530ea6aea91d4e07': '',
-  'streamsComplete': 200
+  'streamsDesired': 500,
+  'streamsCompleted': 500,
+  'streamsStarted': 500,
+  'streamsMeasured': 0
 });
