f4a8421110a7e477ae8c13e1ab301c3b79794506
Gabriel Schulhof <gabriel.schulhof@intel.com>
2019-05-14 23:04:22 -0700

    stream: use immediate more often
    An immediate has to be set whenever the requests queue is empty, even
    if we're not on the last write, because the streaming implementation
    doesn't attach any handles to the event loop.
    
    PR-URL: https://gitlab.devtools.intel.com/RPO/isal_crypto-napi/merge_requests/7
    Reviewed-By: Uttam Pawar <uttam.c.pawar@intel.com>

--- test/index.js
+++ test/index.js
@@ -4,13 +4,10 @@ const { spawnSync } = require('child_process');
 fs.readdirSync(__dirname).forEach((item) => {
   const fullPath = path.join(__dirname, item);
   if (fullPath.match(/[.]js$/) && fullPath !== __filename) {
-    const child = spawnSync(process.execPath, [ fullPath ]);
+    const child = spawnSync(process.execPath, [ fullPath ], {
+      stdio: 'inherit'
+    });
     if (child.status !== 0) {
-      console.log('*** ' + fullPath + ' failed ***');
-      console.log('stdout:');
-      console.log(child.stdout.toString());
-      console.log('stderr:');
-      console.log(child.stderr.toString());
       throw new Error(child.error);
     }
   }
--- test/mb.js
+++ test/mb.js
@@ -10,41 +10,64 @@ const streamCount = {
   streamsCompleted: 500,
 };
 
-const results = {
-  fromFile: {
-    sha256: Object.assign({}, streamCount, {
-      '1feba561bf9106f3cbf6d78dd0c6056eef6ab59f15a30e64530ea6aea91d4e07': ''
-    }),
-    sha512: Object.assign({}, streamCount, {
-      '48628f3646b0adaa86a9f036e52bb21a89b1902023e4be3a3237dae70283140a0d1a8b921c7fa4806097f593cb4f9c8c4dc551b266ba63abe7c2f5409744a20a': ''
-    })
+const fileHashes = {
+  sha256: {
+    '1feba561bf9106f3cbf6d78dd0c6056eef6ab59f15a30e64530ea6aea91d4e07': ''
   },
-  internal: {
-    sha256: Object.assign({}, streamCount, {
-      '30e14955ebf1352266dc2ff8067e68104607e750abb9d3b36582b8af909fcb58': ''
-    }),
-    sha512: Object.assign({}, streamCount, {
-      'd6292685b380e338e025b3415a90fe8f9d39a46e7bdba8cb78c50a338cefca741f69e4e46411c32de1afdedfb268e579a51f81ff85e56f55b0ee7c33fe8c25c9': ''
-    })
-  },
+  sha512:  {
+    ['48628f3646b0adaa86a9f036e52bb21a89b1902023e4be3a3237dae70283140a0d1a8b9' +
+      '21c7fa4806097f593cb4f9c8c4dc551b266ba63abe7c2f5409744a20a']: ''
+  }
 };
 
-[true, false].forEach((runNodeJS) => {
-  Object.keys(results).forEach((runTypeKey) => {
-    Object.keys(results[runTypeKey]).forEach((hash) => {
+const internalHashes = {
+  sha256: {
+    '30e14955ebf1352266dc2ff8067e68104607e750abb9d3b36582b8af909fcb58': ''
+  },
+  sha512: {
+    ['d6292685b380e338e025b3415a90fe8f9d39a46e7bdba8cb78c50a338cefca741f69e4e' +
+      '46411c32de1afdedfb268e579a51f81ff85e56f55b0ee7c33fe8c25c9']: ''
+  }
+};
+
+Object.keys(fileHashes).forEach((hash) => {
+  const child = spawnSync(process.execPath, [
+    benchmarkPath,
+    JSON.stringify({
+      runNodeJS: false,
+      hash,
+      runAsTest: true,
+      fromFile: true,
+      streamsToStart: 3
+    })
+  ]);
+  console.log('few ' + hash + ' mb streams');
+  assert.deepStrictEqual(JSON.parse(child.stdout.toString()),
+    Object.assign({}, {
+      streamsDesired: 3,
+      streamsStarted: 3,
+      streamsCompleted: 3
+    }, fileHashes[hash]));
+});
+
+[false, true].forEach((runNodeJS) => {
+  [true, false].forEach((fromFile) => {
+    Object.keys(fileHashes).forEach((hash) => {
       const child = spawnSync(process.execPath, [
         benchmarkPath, JSON.stringify(Object.assign({
           runNodeJS,
           hash,
+          fromFile,
           runAsTest: true,
-          fromFile: (runTypeKey === 'fromFile')
-        }, (runTypeKey === 'fromFile') ? {} : {
+        }, fromFile ? {} : {
           streamLength: 1048576
         }))
       ]);
-      console.log(runTypeKey, (runNodeJS ? 'nodejs' : 'mb'), hash);
+      console.log((fromFile ? 'file': 'internal'), (runNodeJS ? 'nodejs' : 'mb'), hash);
       assert.deepStrictEqual(
-        JSON.parse(child.stdout.toString()), results[runTypeKey][hash]);
+        JSON.parse(child.stdout.toString()),
+        Object.assign({}, streamCount,
+          (fromFile ? fileHashes[hash] : internalHashes[hash])));
     });
   });
 });
