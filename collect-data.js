ec081157fce641484031bc2fd618be9d3a47fe30
Gabriel Schulhof <gabriel.schulhof@intel.com>
2019-05-15 10:33:10 -0700

    collect-data: interleave, discard 5%, and average
    PR-URL: https://gitlab.devtools.intel.com/RPO/isal_crypto-napi/merge_requests/11
    Reviewed-By: Uttam Pawar <uttam.c.pawar@intel.com>

--- collect-data.js
+++ collect-data.js
@@ -3,21 +3,41 @@ const { spawnSync, spawn } = require('child_process');
 const benchmarkPath = require('path').join(__dirname, 'benchmark', 'stream');
 const hashes = [ 'sha256', 'sha512' ];
 const samples = 20;
+const tailSizePercent = 0.05;
 
-function getOneAverage(hash, runNodeJS) {
-  let result = 0;
+function measureOneHash(hash) {
+  let resultsNode = [];
+  let resultsMB = [];
   for (let idx = 0; idx < samples; idx++) {
-    process.stdout.write('\r\033[K' + hash + (runNodeJS ? '(node)' : '') +
-      ': iteration ' + idx);
-    result += JSON.parse(
+    process.stdout.write('\r\033[K' + hash + '(node): iteration ' + idx);
+    resultsNode.push(JSON.parse(
       spawnSync(process.execPath, [
-          benchmarkPath, JSON.stringify({hash, runNodeJS})
+          benchmarkPath, JSON.stringify({hash, runNodeJS: true})
       ])
         .stdout
         .toString())
-      .elapsed;
+      .elapsed);
+    process.stdout.write('\r\033[K' + hash + ': iteration ' + idx);
+    resultsMB.push(JSON.parse(
+      spawnSync(process.execPath, [
+          benchmarkPath, JSON.stringify({hash, runNodeJS: false})
+      ])
+        .stdout
+        .toString())
+      .elapsed);
   }
-  return (result / samples);
+
+  return (
+    resultsNode
+      .sort()
+      .slice(Math.round(samples * tailSizePercent),
+        Math.round(samples * (1 - tailSizePercent)))
+      .reduce((current, item) => (current + item)) /
+    resultsMB
+      .sort()
+      .slice(Math.round(samples * tailSizePercent),
+        Math.round(samples * (1 - tailSizePercent)))
+      .reduce((current, item) => (current + item)));
 }
 
 if (process.argv[2] === 'rebuild') {
@@ -36,6 +56,5 @@ if (process.argv[2] === 'rebuild') {
 }
 
 hashes.forEach((hash) => {
-  console.log('\r\033[K' + hash + ':\t' +
-    getOneAverage(hash, true) / getOneAverage(hash, false));
+  console.log('\r\033[K' + hash + ':\t' + measureOneHash(hash));
 });
